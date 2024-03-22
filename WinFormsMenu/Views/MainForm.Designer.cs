6924644f982d5bd30acea2e5601a330f1b229bc5
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-03-22 22:48:28 +0200

    10. Connected logic to Interface of menu for loading of WebPages from SQL server db.

--- WinFormsMenu/Views/MainForm.Designer.cs
+++ WinFormsMenu/Views/MainForm.Designer.cs
@@ -63,6 +63,7 @@
             mnConnection.Text = "Connection";
             // 
             // mnDefault
+            //
             // 
             mnDefault.DropDownItems.AddRange(new ToolStripItem[] { mnLoad });
             mnDefault.Name = "mnDefault";
